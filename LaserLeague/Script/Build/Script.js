9fd922a7cff517c46e580d2c13ad45ada2bbdace
Daniel Schlegel <d.schlegel@mediadudes.de>
2021-10-19 15:12:29 +0200

    add simple agent movement

--- LaserLeague/Script/Build/Script.js
+++ LaserLeague/Script/Build/Script.js
@@ -38,18 +38,36 @@ var Script;
     f.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
-    let laser_1;
+    let laser_1Transform;
+    let agentTransform;
+    //let camera: f.ComponentCamera;
     function start(_event) {
         viewport = _event.detail;
-        f.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        f.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         let graph = viewport.getBranch();
         let lasers = graph.getChildrenByName("Lasers");
-        laser_1 = lasers[0].getChildrenByName("Laser_1")[0];
+        let agents = graph.getChildrenByName("Agents");
+        //camera = graph.getChildrenByName("Camera")[0].getComponent(f.ComponentCamera);
+        laser_1Transform = lasers[0].getChildrenByName("Laser_1")[0].getComponent(f.ComponentTransform).mtxLocal;
+        agentTransform = agents[0].getChildrenByName("Agent_1")[0].getComponent(f.ComponentTransform).mtxLocal;
+        //viewport.camera = camera;
+        f.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        f.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // f.Physics.world.simulate();  // if physics is included and use
-        laser_1.getComponent(f.ComponentTransform).mtxLocal.rotateZ(1);
+        laser_1Transform.rotateZ(1);
+        if (f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_UP])) {
+            agentTransform.translateY(0.1);
+        }
+        if (f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_DOWN])) {
+            agentTransform.translateY(-0.1);
+        }
+        if (f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_LEFT])) {
+            agentTransform.rotateZ(5);
+        }
+        if (f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_RIGHT])) {
+            agentTransform.rotateZ(-5);
+        }
         viewport.draw();
         f.AudioManager.default.update();
     }
--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -5,23 +5,44 @@ namespace Script {
   let viewport: f.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
-  let laser_1: f.Node;
+  let laser_1Transform: f.Matrix4x4;
+  let agentTransform: f.Matrix4x4;
+  //let camera: f.ComponentCamera;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
-    f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
-    f.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-
     let graph: f.Node = viewport.getBranch();
     let lasers: f.Node[] = graph.getChildrenByName("Lasers");
-    laser_1 = lasers[0].getChildrenByName("Laser_1")[0]; 
-    
+    let agents: f.Node[] = graph.getChildrenByName("Agents");
+    //camera = graph.getChildrenByName("Camera")[0].getComponent(f.ComponentCamera);
+    laser_1Transform = lasers[0].getChildrenByName("Laser_1")[0].getComponent(f.ComponentTransform).mtxLocal; 
+    agentTransform = agents[0].getChildrenByName("Agent_1")[0].getComponent(f.ComponentTransform).mtxLocal;
+
+
+    //viewport.camera = camera;
+
+    f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
+    f.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // f.Physics.world.simulate();  // if physics is included and use
-    laser_1.getComponent(f.ComponentTransform).mtxLocal.rotateZ(1);
+    laser_1Transform.rotateZ(1);
+    
+    if(f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_UP])) {
+      agentTransform.translateY(0.1);
+    }
+    if(f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_DOWN])) {
+      agentTransform.translateY(-0.1);
+    }
+    if(f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_LEFT])) {
+      agentTransform.rotateZ(5);
+    }
+    if(f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_RIGHT])) {
+      agentTransform.rotateZ(-5);
+    }
+
     viewport.draw();
     f.AudioManager.default.update();
   }
