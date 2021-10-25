aec24379968f6ad752de60200b360f05f82f21f5
Daniel Schlegel <45203036+dwiedani@users.noreply.github.com>
2021-10-25 15:58:55 +0200

    add more lasers and fix collision
    + player reset when collison happens

--- LaserLeague/Script/Build/Script.js
+++ LaserLeague/Script/Build/Script.js
@@ -38,11 +38,12 @@ var Script;
     f.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
-    let laser_1;
-    let laser_1Transform;
-    let laser_1RotationSpeed = 270;
+    let laserRotationSpeed = 120;
+    let lasers;
     let agent;
+    let agentCanMove;
     let agentTransform;
+    let agentStartPosition = new f.Vector3(0, 0, 0);
     let agentMaxMovementSpeed = 7.0;
     let agentMaxTurnSpeed = 270;
     let agentControlForward = new f.Control("Forward", 1, 0 /* PROPORTIONAL */);
@@ -51,10 +52,9 @@ var Script;
     function start(_event) {
         viewport = _event.detail;
         let graph = viewport.getBranch();
-        let lasers = graph.getChildrenByName("Lasers");
+        let laserCollection = graph.getChildrenByName("Lasers")[0];
+        lasers = laserCollection.getChildrenByName("Laser");
         let agents = graph.getChildrenByName("Agents");
-        laser_1 = lasers[0].getChildrenByName("Laser_1")[0];
-        laser_1Transform = laser_1.getComponent(f.ComponentTransform).mtxLocal;
         agent = agents[0].getChildrenByName("Agent_1")[0];
         agentTransform = agent.getComponent(f.ComponentTransform).mtxLocal;
         viewport.camera.mtxPivot.translateZ(-15);
@@ -62,28 +62,48 @@ var Script;
         f.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         f.Loop.start(f.LOOP_MODE.TIME_REAL, 60); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         alert("Use arrowkeys for movement!");
+        agentCanMove = true;
     }
     function update(_event) {
         //f.Physics.world.simulate();  // if physics is included and use
-        laser_1Transform.rotateZ(laser_1RotationSpeed * f.Loop.timeFrameReal / 1000);
         let ForwardValue = (f.Keyboard.mapToValue(-1, 0, [f.KEYBOARD_CODE.ARROW_DOWN]) + f.Keyboard.mapToValue(1, 0, [f.KEYBOARD_CODE.ARROW_UP]));
         let TurnValue = (f.Keyboard.mapToValue(-1, 0, [f.KEYBOARD_CODE.ARROW_RIGHT]) + f.Keyboard.mapToValue(1, 0, [f.KEYBOARD_CODE.ARROW_LEFT]));
-        agentControlForward.setInput(ForwardValue);
-        agentControlTurn.setInput(TurnValue);
-        agentTransform.rotateZ(agentControlTurn.getOutput() * agentMaxTurnSpeed * f.Loop.timeFrameReal / 1000);
-        agentTransform.translateY(agentControlForward.getOutput() * agentMaxMovementSpeed * f.Loop.timeFrameReal / 1000);
-        //console.log(f.Vector3.TRANSFORMATION(agent.mtxWorld.translation, laser_1.mtxWorldInverse,true).toString());
-        checkCollision(agent, laser_1.getChildrenByName("LaserArm_1")[0]);
+        if (agentCanMove) {
+            agentControlForward.setInput(ForwardValue);
+            agentControlTurn.setInput(TurnValue);
+            agentTransform.rotateZ(agentControlTurn.getOutput() * agentMaxTurnSpeed * f.Loop.timeFrameReal / 1000);
+            agentTransform.translateY(agentControlForward.getOutput() * agentMaxMovementSpeed * f.Loop.timeFrameReal / 1000);
+        }
+        //console.log(f.Vector3.TRANSFORMATION(agent.mtxWorld.translation, laser.mtxWorldInverse,true).toString());
+        lasers.forEach(laser => {
+            laser.getComponent(f.ComponentTransform).mtxLocal.rotateZ(laserRotationSpeed * f.Loop.timeFrameReal / 1000);
+            let laserBeams = laser.getChildrenByName("LaserArm");
+            laserBeams.forEach(beam => {
+                checkCollision(agent, beam);
+            });
+        });
         viewport.draw();
         f.AudioManager.default.update();
     }
     function checkCollision(collider, obstacle) {
         let distance = f.Vector3.TRANSFORMATION(collider.mtxWorld.translation, obstacle.mtxWorldInverse, true);
         let minX = obstacle.getComponent(f.ComponentMesh).mtxPivot.scaling.x / 2 + collider.radius;
-        let minY = obstacle.getComponent(f.ComponentMesh).mtxPivot.scaling.y / 2 + collider.radius;
-        if (distance.x <= (minX) && distance.x >= -(minX) && distance.y <= minY && distance.y >= -(minY)) {
-            alert("Collision happened!");
+        let minY = obstacle.getComponent(f.ComponentMesh).mtxPivot.scaling.y + collider.radius;
+        if (distance.x <= (minX) && distance.x >= -(minX) && distance.y <= minY && distance.y >= 0) {
+            respawn();
         }
+    }
+    function respawn() {
+        agentTransform.translate(f.Vector3.TRANSFORMATION(agentStartPosition, agent.mtxWorldInverse, true));
+        agentCanMove = false;
+        agentControlForward.setDelay(0);
+        agentControlForward.setInput(0);
+        agentControlTurn.setInput(0);
+        agentControlForward.setDelay(500);
+        setTimeout(() => {
+            agentCanMove = true;
+            agentControlForward.setDelay(500);
+        }, 500);
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -4,13 +4,12 @@ namespace Script {
 
   let viewport: f.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
-
-  let laser_1: f.Node;
-  let laser_1Transform: f.Matrix4x4;
-  let laser_1RotationSpeed: number = 270;
-
+  let laserRotationSpeed: number = 120;
+  let lasers: f.Node[];
   let agent: f.Node;
+  let agentCanMove: boolean;
   let agentTransform: f.Matrix4x4;
+  let agentStartPosition: f.Vector3 = new f.Vector3(0,0,0);
   let agentMaxMovementSpeed: number = 7.0;
   let agentMaxTurnSpeed: number = 270;
   let agentControlForward: f.Control = new f.Control("Forward", 1, f.CONTROL_TYPE.PROPORTIONAL);
@@ -19,17 +18,12 @@ namespace Script {
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-
     let graph: f.Node = viewport.getBranch();
-    let lasers: f.Node[] = graph.getChildrenByName("Lasers");
+    let laserCollection: f.Node = graph.getChildrenByName("Lasers")[0];
+    lasers = laserCollection.getChildrenByName("Laser");
     let agents: f.Node[] = graph.getChildrenByName("Agents");
-
-    laser_1 = lasers[0].getChildrenByName("Laser_1")[0];
-    laser_1Transform = laser_1.getComponent(f.ComponentTransform).mtxLocal; 
-
     agent = agents[0].getChildrenByName("Agent_1")[0];
     agentTransform = agent.getComponent(f.ComponentTransform).mtxLocal;
-
     viewport.camera.mtxPivot.translateZ(-15);
 
     //viewport.camera = camera;
@@ -37,11 +31,11 @@ namespace Script {
     f.Loop.start(f.LOOP_MODE.TIME_REAL, 60);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
 
     alert("Use arrowkeys for movement!");
+    agentCanMove = true;
   }
 
   function update(_event: Event): void {
     //f.Physics.world.simulate();  // if physics is included and use
-    laser_1Transform.rotateZ(laser_1RotationSpeed * f.Loop.timeFrameReal / 1000 );
 
     let ForwardValue: number = (
       f.Keyboard.mapToValue(-1, 0, [f.KEYBOARD_CODE.ARROW_DOWN]) + f.Keyboard.mapToValue(1, 0, [f.KEYBOARD_CODE.ARROW_UP])
@@ -50,16 +44,24 @@ namespace Script {
     let TurnValue: number = (
       f.Keyboard.mapToValue(-1, 0, [f.KEYBOARD_CODE.ARROW_RIGHT]) + f.Keyboard.mapToValue(1, 0, [f.KEYBOARD_CODE.ARROW_LEFT])
     );
-
-    agentControlForward.setInput(ForwardValue);
-    agentControlTurn.setInput(TurnValue);
-
-    agentTransform.rotateZ(agentControlTurn.getOutput() * agentMaxTurnSpeed * f.Loop.timeFrameReal / 1000);
-
-    agentTransform.translateY(agentControlForward.getOutput() * agentMaxMovementSpeed * f.Loop.timeFrameReal / 1000);
+    if(agentCanMove) {
+      agentControlForward.setInput(ForwardValue);
+      agentControlTurn.setInput(TurnValue);
+      agentTransform.rotateZ(agentControlTurn.getOutput() * agentMaxTurnSpeed * f.Loop.timeFrameReal / 1000);
+      agentTransform.translateY(agentControlForward.getOutput() * agentMaxMovementSpeed * f.Loop.timeFrameReal / 1000);
+    }
     
-    //console.log(f.Vector3.TRANSFORMATION(agent.mtxWorld.translation, laser_1.mtxWorldInverse,true).toString());
-    checkCollision(agent,laser_1.getChildrenByName("LaserArm_1")[0]);
+    //console.log(f.Vector3.TRANSFORMATION(agent.mtxWorld.translation, laser.mtxWorldInverse,true).toString());
+
+    lasers.forEach(laser => {
+      laser.getComponent(f.ComponentTransform).mtxLocal.rotateZ(laserRotationSpeed * f.Loop.timeFrameReal / 1000);
+      let laserBeams: f.Node[] = laser.getChildrenByName("LaserArm");
+      laserBeams.forEach(beam => {
+        checkCollision(agent,beam);
+      });
+    });
+    
+    
     viewport.draw();
     f.AudioManager.default.update();
   }
@@ -67,10 +69,23 @@ namespace Script {
   function checkCollision(collider:f.Node, obstacle: f.Node) {
     let distance: f.Vector3 = f.Vector3.TRANSFORMATION(collider.mtxWorld.translation, obstacle.mtxWorldInverse,true);   
     let minX = obstacle.getComponent(f.ComponentMesh).mtxPivot.scaling.x/2 + collider.radius;
-    let minY = obstacle.getComponent(f.ComponentMesh).mtxPivot.scaling.y/2 + collider.radius;
-    if(distance.x <= (minX) && distance.x >= -(minX) && distance.y <= minY && distance.y >= -(minY)) {
-      alert("Collision happened!");
+    let minY = obstacle.getComponent(f.ComponentMesh).mtxPivot.scaling.y + collider.radius;
+    if(distance.x <= (minX) && distance.x >= -(minX) && distance.y <= minY && distance.y >= 0) {
+      respawn()
     }
+  }
+
+  function respawn(){
+    agentTransform.translate(f.Vector3.TRANSFORMATION(agentStartPosition,agent.mtxWorldInverse,true));
+    agentCanMove = false;
+    agentControlForward.setDelay(0);
+    agentControlForward.setInput(0);
+    agentControlTurn.setInput(0);
+    agentControlForward.setDelay(500);
+    setTimeout(()=>{
+      agentCanMove = true;
+      agentControlForward.setDelay(500);
+    },500);
   }
 
 }
